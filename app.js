870ca92bbb7a6446f1bd1ec74297f2def769228f
Matthew Harrison-Jones <matthojo@gmail.com>
2014-03-26 12:58:15 +0000

    Refactor widget base

--- app.js
+++ app.js
@@ -3,32 +3,9 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , http = require('http')
-  , path = require('path')
-  , app = express()
+var Widget = new require('hud-widget')
+  , widget = new Widget()
 
-// all environments
-app.set('port', process.env.PORT || 3000)
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'jade')
-app.use(express.favicon())
-app.use(express.logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(express.methodOverride())
-app.use(app.router)
-app.use(express.static(path.join(__dirname, 'public')))
-
-// development only
-if ('development' === app.get('env')) {
-  app.use(express.errorHandler())
-}
-
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Express' })
-})
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'))
+widget.get('/', function (req, res) {
+  res.render('index', { title: 'Widget Template' })
 })
